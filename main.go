82562789af2cb3a445a2e03993716b130c400a58
dyb <dyb@slz-windows>
2021-08-25 18:05:11 +0800

    自定义日志格式

--- main.go
+++ main.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	"github.com/aidreamwin/gomlib/disk"
-	"log"
+	"github.com/aidreamwin/gomlib/log"
 )
 
 func main() {
-	log.Println("gomlib")
+	log.SetFlags(log.Lshortfile | log.LstdFlags)
+	log.Trace("gomlib", "ddddd有意思")
+	log.Debug("gomlib")
+	log.Info("gomlib")
+	log.Warn("gomlib")
+	log.Error("gomlib")
 	lpFreeBytesAvailable, lpTotalNumberOfBytes, lpTotalNumberOfFreeBytes, err := disk.DiskUsage("C:")
 	if err != nil {
 		log.Println(err)
